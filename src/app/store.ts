4b31f01f60487845e9b3f6a9f3265bd3ada1b02a
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2024-07-21 00:58:30 +0300

    feat: add user slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,15 +3,20 @@ import { productsApi } from '@/entities/product/api';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { productsSlice } from '@/entities/product';
 import { cartSlice } from '@/entities/cart';
+import { userSlice } from '@/entities/user/model/userSlice';
+import { userApi } from '@/entities/user/api';
 
 export const store = configureStore({
   reducer: {
     cart: cartSlice.reducer,
     products: productsSlice.reducer,
     [productsApi.reducerPath]: productsApi.reducer,
+    user: userSlice.reducer,
+    [userApi.reducerPath]: userApi.reducer,
   },
 
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productsApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware, userApi.middleware),
 });
 
 setupListeners(store.dispatch);
