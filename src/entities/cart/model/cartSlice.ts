8dc6735ec2732fb3d4daaac3a80b32bced853377
Elena Pogodaeva <pogodaeva_e@inbox.ru>
2024-07-21 17:33:53 +0300

    feat: add reducers for update cart

--- src/entities/cart/model/cartSlice.ts
+++ src/entities/cart/model/cartSlice.ts
@@ -1,6 +1,7 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ICart } from '@/entities/cart/model/types';
-import { fetchCarts } from './cartThunk';
+import { calcDiscountPrice } from '@/shared/lib/price';
+import { fetchCarts, updateUserCard } from './cartThunk';
 
 export type CartState = {
   cart: ICart | null;
@@ -17,7 +18,27 @@ const initialState: CartState = {
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
+  reducers: {
+    deleteProduct(state, action: PayloadAction<number>) {
+      state.cart!.products = state.cart!.products.filter((item) => item.id !== action.payload);
+    },
+    calcTotal(state) {
+      state.cart!.total = +state
+        .cart!.products.reduce((sum, curr) => sum + curr.total, 0)
+        .toFixed(2);
+      state.cart!.discountedTotal = +state
+        .cart!.products.reduce((sum, curr) => {
+          const discountPrice = calcDiscountPrice(curr.total, curr.discountPercentage);
+          return sum + +discountPrice;
+        }, 0)
+        .toFixed(2);
+      state.cart!.totalProducts = state.cart!.products.length;
+      state.cart!.totalQuantity = state.cart!.products.reduce(
+        (sum, curr) => sum + curr.quantity,
+        0
+      );
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchCarts.pending, (state) => {
       state.isLoading = true;
@@ -33,5 +54,29 @@ export const cartSlice = createSlice({
       state.error = 'Error occured';
       state.cart = null;
     });
+    builder.addCase(updateUserCard.pending, (state) => {
+      state.isLoading = true;
+      state.error = '';
+    });
+    builder.addCase(updateUserCard.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.error = '';
+
+      const [product] = action.payload.products;
+
+      const productIndex = state.cart!.products.findIndex((item) => item.id === product.id);
+
+      if (productIndex !== -1) {
+        state.cart!.products[productIndex] = product;
+      } else {
+        state.cart!.products.push(product);
+      }
+    });
+    builder.addCase(updateUserCard.rejected, (state) => {
+      state.isLoading = false;
+      state.error = 'Error occured';
+    });
   },
 });
+
+export const { deleteProduct, calcTotal } = cartSlice.actions;
